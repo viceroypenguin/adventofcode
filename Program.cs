703e11d6e80ef8c561b3a44bc4e3e860a1496b97
Stuart Turner <stuart@turner-isageek.com>
2019-03-11 21:28:55 -0500

    Add command lines

--- Program.cs
+++ Program.cs
@@ -3,48 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
+using MoreLinq;
 
 namespace AdventOfCode
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Pre-JIT Day and Stopwatch
-            new DummyDay().Execute();
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Pre-JIT Day and Stopwatch
+			new DummyDay().Execute();
 
-            var days = GetDays();
-            if (args.Contains("-o"))
-                days = days.Where(d => d.CodeType == CodeType.Original);
-            else if (args.Contains("-f"))
-                days = days.Where(d => d.CodeType == CodeType.Fastest);
+			var days = GetDays();
+			if (args.Contains("-o"))
+				days = days.Where(d => d.CodeType == CodeType.Original);
+			else if (args.Contains("-f"))
+				days = days.Where(d => d.CodeType == CodeType.Fastest);
 
-            foreach (var d in days)
-                d.Execute();
+			if (args.Contains("-y"))
+			{
+				var year = Convert.ToInt32(args[args.Index().Single(a => a.Value == "-y").Key + 1]);
+				days = days.Where(d => d.Year == year);
+			}
 
-            Console.WriteLine();
+			if (args.Contains("-d"))
+			{
+				var day = Convert.ToInt32(args[args.Index().Single(a => a.Value == "-d").Key + 1]);
+				days = days.Where(d => d.DayNumber == day);
+			}
 
-            foreach (var g in days.GroupBy(d => new { d.Year, d.CodeType, }))
-            {
-                Console.WriteLine(
-                    string.Join(Environment.NewLine,
-                        g.Select(d => $"Year {d.Year}, Day {d.DayNumber,2}, Type {d.CodeType,9}      :   {d.TotalMicroseconds,13:N0} μs")));
+			foreach (var d in days)
+				d.Execute();
 
-                Console.WriteLine("--------------------------------------------------------");
-                Console.WriteLine(
-                    $"Year {g.Key.Year},         Type {g.Key.CodeType,9} Total:   {g.Sum(d => d.TotalMicroseconds),13:N0} μs");
-                Console.WriteLine();
-            }
-        }
+			Console.WriteLine();
 
-        private static IEnumerable<Day> GetDays() =>
-            Assembly.GetExecutingAssembly().GetTypes()
-                .Where(t => t.BaseType == typeof(Day))
-                .Where(t => t.Name != nameof(DummyDay))
-                .Select(t => (Day)Activator.CreateInstance(t))
-                .OrderBy(d => d.Year)
-                .ThenBy(d => d.DayNumber)
-                .ThenBy(d => d.CodeType)
-                .ToArray();
-    }
+			foreach (var g in days.GroupBy(d => new { d.Year, d.CodeType, }))
+			{
+				Console.WriteLine(
+					string.Join(Environment.NewLine,
+						g.Select(d => $"Year {d.Year}, Day {d.DayNumber,2}, Type {d.CodeType,9}      :   {d.TotalMicroseconds,13:N0} μs")));
+
+				Console.WriteLine("--------------------------------------------------------");
+				Console.WriteLine(
+					$"Year {g.Key.Year},         Type {g.Key.CodeType,9} Total:   {g.Sum(d => d.TotalMicroseconds),13:N0} μs");
+				Console.WriteLine();
+			}
+		}
+
+		private static IEnumerable<Day> GetDays() =>
+			Assembly.GetExecutingAssembly().GetTypes()
+				.Where(t => t.BaseType == typeof(Day))
+				.Where(t => t.Name != nameof(DummyDay))
+				.Select(t => (Day)Activator.CreateInstance(t))
+				.OrderBy(d => d.Year)
+				.ThenBy(d => d.DayNumber)
+				.ThenBy(d => d.CodeType)
+				.ToArray();
+	}
 }
