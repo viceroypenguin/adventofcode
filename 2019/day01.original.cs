c73e912cb28c87e40f7618df64aa484df08b68d6
Stuart Turner <stuart@turner-isageek.com>
2019-11-30 23:11:37 -0600

    Y19 D01

---
+++ 2019/day01.original.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+	public class Day_2019_01_Original : Day
+	{
+		public override int Year => 2019;
+		public override int DayNumber => 1;
+		public override CodeType CodeType => CodeType.Original;
+
+		protected override void ExecuteDay(byte[] input)
+		{
+			if (input == null) return;
+
+			static IEnumerable<int> fuelValues(int start) =>
+				MoreEnumerable.Generate(start, s => Math.Max(s / 3 - 2, 0))
+					.Skip(1)
+					.TakeWhile(s => s != 0);
+
+			var numbers = input
+				.GetLines()
+				.Select(s => Convert.ToInt32(s))
+				.ToList();
+
+			PartA = numbers
+				.Select(s => fuelValues(s).First())
+				.Sum()
+				.ToString();
+
+			PartB = numbers
+				.Select(s => fuelValues(s).Sum())
+				.Sum()
+				.ToString();
+		}
+	}
+}
