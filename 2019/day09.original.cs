cfb8ca1aef952675dfadab4bfef567a938602a45
Stuart Turner <stuart@turner-isageek.com>
2019-12-09 13:44:24 -0600

    Y19 D09

---
+++ 2019/day09.original.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+	public class Day_2019_09_Original : Day
+	{
+		public override int Year => 2019;
+		public override int DayNumber => 9;
+		public override CodeType CodeType => CodeType.Original;
+
+		protected override void ExecuteDay(byte[] input)
+		{
+			if (input == null) return;
+
+			var instructions = input.GetString()
+				.Split(',')
+				.Select(long.Parse)
+				.ToArray();
+
+			// 640k should be enough for anyone
+			Array.Resize(ref instructions, 640 * 1024);
+
+			var inputs = new BufferBlock<long>();
+			inputs.Post(1);
+			var outputs = new BufferBlock<long>();
+			var pc = new IntCodeComputer(instructions.ToArray(), inputs, outputs);
+			pc.RunProgram()
+				.GetAwaiter()
+				.GetResult();
+			PartA = outputs.Receive().ToString();
+
+			inputs = new BufferBlock<long>();
+			inputs.Post(2);
+			outputs = new BufferBlock<long>();
+			pc = new IntCodeComputer(instructions.ToArray(), inputs, outputs);
+			pc.RunProgram()
+				.GetAwaiter()
+				.GetResult();
+			PartB = outputs.Receive().ToString();
+		}
+	}
+}
