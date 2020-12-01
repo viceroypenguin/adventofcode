e06b5c71216854a2d3c3b642c7e5e2003f22b7c4
Stuart Turner <stuart@turner-isageek.com>
2020-12-01 08:30:37 -0600

    Y20 D01

---
+++ 2020/day01.original.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+	public class Day_2020_01_Original : Day
+	{
+		public override int Year => 2020;
+		public override int DayNumber => 1;
+		public override CodeType CodeType => CodeType.Original;
+
+		protected override void ExecuteDay(byte[] input)
+		{
+			if (input == null) return;
+
+			var numbers = input.GetLines()
+				.Select(x => int.Parse(x))
+				.ToArray();
+
+			var max = 2020 - numbers.Min();
+
+			var (x, y, z) = (
+				from _x in numbers
+				where _x <= max
+				from _y in numbers
+				where _x + _y == 2020
+				select (_x, _y, 0)).First();
+
+			PartA = (x * y).ToString();
+
+			(x, y, z) = (
+				from _x in numbers
+				where _x <= max
+				from _y in numbers
+				where _x + _y <= max
+				from _z in numbers
+				where _x + _y + _z == 2020
+				select (_x, _y, _z)).First();
+
+			PartB = (x * y * z).ToString();
+		}
+	}
+}
