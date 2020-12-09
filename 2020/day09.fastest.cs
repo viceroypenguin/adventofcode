2df72f09a96425751ab7f0047e2cd16ee3ec49f7
Stuart Turner <stuart@turner-isageek.com>
2020-12-09 17:49:33 -0600

    Y20 D09 F

---
+++ 2020/day09.fastest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+	public class Day_2020_09_Fastest : Day
+	{
+		public override int Year => 2020;
+		public override int DayNumber => 9;
+		public override CodeType CodeType => CodeType.Fastest;
+
+		protected override void ExecuteDay(byte[] input)
+		{
+			if (input == null) return;
+
+			var span = new ReadOnlySpan<byte>(input);
+			Span<long> arr = stackalloc long[input.Length / 8];
+			int maxIndex = 0;
+
+			for (int i = 0; i < span.Length;)
+			{
+				var (x, y) = span[i..].AtoL();
+				arr[maxIndex++] = x;
+				i += y + 1; // plus 1 to ignore next char
+			}
+
+			var invalidNumber = 0L;
+			for (int i = 25; i < maxIndex; i++)
+			{
+				for (int j = i - 25; j < i; j++)
+					for (int k = j + 1; k < i; k++)
+						if (arr[j] + arr[k] == arr[i])
+							goto found_match;
+
+				PartA = (invalidNumber = arr[i]).ToString();
+				break;
+
+found_match:
+				;
+			}
+
+			for (int i = maxIndex - 1; i >= 0; i--)
+			{
+				var x = arr[i];
+				if (x > invalidNumber)
+					continue;
+
+				var (sum, min, max) = (x, x, x);
+				for (int j = i - 1; j >= 0; j++)
+				{
+					sum += arr[j];
+					min = Math.Min(min, arr[j]);
+					max = Math.Max(max, arr[j]);
+					if (sum == invalidNumber)
+					{
+						PartB = (min + max).ToString();
+						return;
+					}
+
+					if (sum > invalidNumber)
+						break;
+				}
+			}
+		}
+	}
+}
--- Helpers.cs
+++ Helpers.cs
@@ -22,7 +22,7 @@ namespace AdventOfCode
 			min.CompareTo(value) <= 0 && value.CompareTo(max) <= 0;
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-		public static unsafe (int value, int numChars) AtoI(this ReadOnlySpan<byte> bytes)
+		public static (int value, int numChars) AtoI(this ReadOnlySpan<byte> bytes)
 		{
 			// initial sign? also, track negative
 			var isNegSign = bytes[0] == '-' ? -1 : 0;
@@ -97,6 +97,80 @@ namespace AdventOfCode
 
 			var result = scaled.ToScalar();
 			return (result + isNegSign, index);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		public static (long value, int numChars) AtoL(this ReadOnlySpan<byte> bytes)
+		{
+			// initial sign? also, track negative
+			var isNegSign = bytes[0] == '-' ? -1 : 0;
+			var isPosSign = bytes[0] == '+' ? -1 : 0;
+			var index = -(isNegSign + isPosSign);
+
+			// skip leading zeros
+			while (index < bytes.Length && bytes[index] == '0')
+				index++;
+
+			// get string length
+			var start = index;
+			while (index < bytes.Length && bytes[index] is >= (byte)'0' and <= (byte)'9')
+				index++;
+
+			if (index == start)
+				return (0, index);
+
+			// load into xmm register
+			var value = Unsafe.ReadUnaligned<Vector256<byte>>(
+				ref MemoryMarshal.GetReference(bytes[start..]));
+
+			var zero = Vector256<byte>.Zero;
+
+			// broadcast (length - 32) to all bytes
+			var len = Avx2.Shuffle(
+				Vector256.Create((byte)(index - start - 32)),
+				zero);
+			// create shuffle array
+			var shuffle = Avx2.Add(
+				Vector256.Create((byte)
+					31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16,
+					15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0),
+				len);
+
+			// shuffle array reverses items, and only includes valid ones
+			value = Avx2.Shuffle(value, shuffle);
+
+			// subtract the '0'
+			value = Avx2.SubtractSaturate(
+				value,
+				Avx2.Shuffle(
+					Vector256.Create((byte)'0'),
+					zero));
+
+			// unpack to words
+			var lowUnpack = Avx2.UnpackLow(value, zero).AsInt16();
+			var highUnpack = Avx2.UnpackHigh(value, zero).AsInt16();
+
+			// scale each word
+			var mulValue = Vector256.Create(
+				1, 10, 100, 1000, 1, 10, 100, 1000,
+				1, 10, 100, 1000, 1, 10, 100, 1000);
+			var lowScaled = Avx2.MultiplyAddAdjacent(lowUnpack, mulValue);
+			var highScaled = Avx2.MultiplyAddAdjacent(highUnpack, mulValue);
+
+			// horizontally add
+			var scaled = Avx2.HorizontalAdd(lowScaled, highScaled);
+
+			var ints = MemoryMarshal.Cast<Vector256<int>, int>(
+				MemoryMarshal.CreateReadOnlySpan(ref scaled, 1));
+
+			long sum = 0L, factor = 1L;
+			foreach (var i in ints)
+			{
+				sum += i * factor;
+				factor *= 10_000;
+			}
+
+			return (sum + isNegSign, index);
 		}
 	}
 }
