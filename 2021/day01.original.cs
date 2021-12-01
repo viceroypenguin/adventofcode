194660f873bf6f53a6e39eafca9519411bdbf84e
Stuart Turner <stuart@turner-isageek.com>
2021-11-30 23:03:11 -0600

    Y21 D01

---
+++ 2021/day01.original.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace AdventOfCode;
+
+public class Day_2021_01_Original : Day
+{
+	public override int Year => 2021;
+	public override int DayNumber => 1;
+	public override CodeType CodeType => CodeType.Original;
+
+	protected override void ExecuteDay(byte[] input)
+	{
+		if (input == null) return;
+
+		PartA = input.GetLines()
+			.Select(l => Convert.ToInt32(l))
+			.Window(2)
+			.Where(x => x.Last() > x.First())
+			.Count()
+			.ToString();
+
+		PartB = input.GetLines()
+			.Select(l => Convert.ToInt32(l))
+			.Window(3)
+			.Select(x => x.Sum())
+			.Window(2)
+			.Where(x => x.Last() > x.First())
+			.Count()
+			.ToString();
+	}
+}
