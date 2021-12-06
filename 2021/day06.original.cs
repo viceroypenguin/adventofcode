27c15dac5206d83531e1f41abc6d1fce7f9f13ce
Stuart Turner <stuart@turner-isageek.com>
2021-12-05 23:18:47 -0600

    Y21 D06 Improved

--- 2021/day06.original.cs
+++ 2021/day06.original.cs
@@ -12,49 +12,40 @@ public class Day_2021_06_Original : Day
 	{
 		if (input == null) return;
 
-		DoPartA(input);
-		DoPartB(input);
-	}
-
-	private void DoPartA(byte[] input)
-	{
 		var fish = input.GetString()
 			.Split(',')
 			.Select(int.Parse)
-			.ToList();
-
-		for (int i = 0; i < 80; i++)
-		{
-			var newFish = fish.Where(x => x == 0).Select(x => 8).ToList();
-			fish = fish.Select(x => x == 0 ? 6 : x - 1).Concat(newFish).ToList();
-		}
-
-		PartA = fish.Count().ToString();
-	}
-
-	private void DoPartB(byte[] input)
-	{
-		var fish = input.GetString()
-			.Split(',')
-			.Select(int.Parse)
+			// don't keep track of each fish individually
+			// only keep track of how many of each age
 			.GroupBy(x => x)
 			.ToDictionary(g => g.Key, g => (long)g.Count());
 
-		for (int i = 0; i < 256; i++)
-		{
-			var newDict = new Dictionary<int, long>();
-			newDict[8] = fish.GetValueOrDefault(0);
-			newDict[7] = fish.GetValueOrDefault(8);
-			newDict[6] = fish.GetValueOrDefault(0) + fish.GetValueOrDefault(7);
-			newDict[5] = fish.GetValueOrDefault(6);
-			newDict[4] = fish.GetValueOrDefault(5);
-			newDict[3] = fish.GetValueOrDefault(4);
-			newDict[2] = fish.GetValueOrDefault(3);
-			newDict[1] = fish.GetValueOrDefault(2);
-			newDict[0] = fish.GetValueOrDefault(1);
-			fish = newDict;
-		}
+		// handles a single day cycle
+		// each day decrements except for special cases
+		static Dictionary<int, long> DayCycle(Dictionary<int, long> fish) =>
+			new()
+			{
+				// all of the 0 ages go to 8 as new fish
+				[8] = fish.GetValueOrDefault(0),
+				[7] = fish.GetValueOrDefault(8),
+				// 0 ages go to 6, along with 7 ages
+				[6] = fish.GetValueOrDefault(0) + fish.GetValueOrDefault(7),
+				[5] = fish.GetValueOrDefault(6),
+				[4] = fish.GetValueOrDefault(5),
+				[3] = fish.GetValueOrDefault(4),
+				[2] = fish.GetValueOrDefault(3),
+				[1] = fish.GetValueOrDefault(2),
+				[0] = fish.GetValueOrDefault(1),
+			};
 
+		// run first 80 days
+		for (int i = 0; i < 80; i++)
+			fish = DayCycle(fish);
+		PartA = fish.Values.Sum().ToString();
+
+		// run from 80 to 256
+		for (int i = 80; i < 256; i++)
+			fish = DayCycle(fish);
 		PartB = fish.Values.Sum().ToString();
 	}
 }
