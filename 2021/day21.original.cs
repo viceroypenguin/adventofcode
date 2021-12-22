9ca8712f94e6bf4436ea01de1f6300995055b73f
Stuart Turner <stuart@turner-isageek.com>
2021-12-21 22:13:36 -0600

    Y21 D21

--- 2021/day21.original.cs
+++ 2021/day21.original.cs
@@ -36,5 +36,45 @@ public class Day_2021_21_Original : Day
 				break;
 			}
 		}
+
+		var diceCount = (
+				from x in Enumerable.Range(1, 3)
+				from y in Enumerable.Range(1, 3)
+				from z in Enumerable.Range(1, 3)
+				group 1 by x + y + z into a
+				select (pos: a.Key, cnt: a.Count()))
+			.ToList();
+
+		var scores = new List<((int pos1, int pos2, int score1, int score2) state, long count)>()
+		{
+			((lines[0][^1] - '0' - 1, lines[1][^1] - '0' - 1, 0, 0), 1),
+		};
+		while (scores.Any(s => s.state.score1 < 21 && s.state.score2 < 21))
+		{
+			scores = scores
+				.SelectMany(s =>
+				{
+					var ((pos1, pos2, score1, score2), count) = s;
+					if (score1 >= 21)
+						return Enumerable.Repeat((state: (0, 0, 0, 21), count), 1);
+					if (score2 >= 21)
+						return Enumerable.Repeat((state: (0, 0, 21, 0), count), 1);
+
+					return diceCount
+						.Select(p =>
+						{
+							var p1 = (pos1 + p.pos) % 10;
+							var s1 = score1 + p1 + 1;
+							return (state: (pos2, p1, score2, s1), count: count * p.cnt);
+						});
+				})
+				.GroupBy(x => x.state, (s, g) => (s, g.Sum(x => x.count)))
+				.ToList();
+		}
+
+		var wins = scores
+			.GroupBy(s => s.state.score1 >= 21, (b, g) => (b, cnt: g.Sum(x => x.count)))
+			.Max(x => x.cnt);
+		PartB = wins.ToString();
 	}
 }
