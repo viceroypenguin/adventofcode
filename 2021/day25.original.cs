3c6ae42bd0cb4f5bb07a6c7e7e6859beea4447d7
Stuart Turner <stuart@turner-isageek.com>
2021-12-30 12:04:41 -0600

    Y21 D25

---
+++ 2021/day25.original.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode;
+
+public class Day_2021_25_Original : Day
+{
+	public override int Year => 2021;
+	public override int DayNumber => 25;
+	public override CodeType CodeType => CodeType.Original;
+
+	protected override void ExecuteDay(byte[] input)
+	{
+		if (input == null) return;
+
+		var map = input.GetMap();
+
+		var yLength = map.Length;
+		var xLength = map[0].Length;
+
+		bool Step(byte[][] map)
+		{
+			var anyMove = false;
+
+			var moveInstructions = Enumerable.Range(0, yLength)
+				.SelectMany(y => Enumerable.Range(0, xLength)
+					.Where(x => map[y][x] == '>' && map[y][(x + 1) % xLength] == '.')
+					.Select(x => (x, y)))
+				.ToList();
+
+			if (moveInstructions.Any())
+				anyMove = true;
+			foreach (var (x, y) in moveInstructions)
+				(map[y][x], map[y][(x + 1) % xLength]) =
+					((byte)'.', (byte)'>');
+
+			moveInstructions = Enumerable.Range(0, yLength)
+				.SelectMany(y => Enumerable.Range(0, xLength)
+					.Where(x => map[y][x] == 'v' && map[(y + 1) % yLength][x] == '.')
+					.Select(x => (x, y)))
+				.ToList();
+
+			if (moveInstructions.Any())
+				anyMove = true;
+			foreach (var (x, y) in moveInstructions)
+				(map[y][x], map[(y + 1) % yLength][x]) =
+					((byte)'.', (byte)'v');
+
+			return anyMove;
+		}
+
+		var cnt = 1;
+		while (Step(map))
+			cnt++;
+
+		PartA = cnt.ToString();
+	}
+}
