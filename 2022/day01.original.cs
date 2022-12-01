91140f1548b3aa92ec51e9c03e16907b41b44735
Stuart Turner <stuart@turner-isageek.com>
2022-11-30 23:09:22 -0600

    Y22 D01

---
+++ 2022/day01.original.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace AdventOfCode;
+
+public class Day_2022_01_Original : Day
+{
+	public override int Year => 2022;
+	public override int DayNumber => 1;
+	public override CodeType CodeType => CodeType.Original;
+
+	protected override void ExecuteDay(byte[] input)
+	{
+		if (input == null) return;
+
+		PartA = input.GetLines(StringSplitOptions.None)
+			.Split(string.Empty)
+			.Select(g => g.Select(int.Parse).Sum())
+			.Max().ToString();
+
+		PartB = input.GetLines(StringSplitOptions.None)
+			.Split(string.Empty)
+			.Select(g => g.Select(int.Parse).Sum())
+			.PartialSort(3, OrderByDirection.Descending)
+			.Sum()
+			.ToString();
+	}
+}
