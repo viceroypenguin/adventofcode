e2b93deae9058d193dd02b1b1b2531fb3904ff75
Stuart Turner <stuart@turner-isageek.com>
2022-12-01 20:08:16 -0600

    Add 2022 Puzzles project

---
+++ AdventOfCode.Puzzles/2022/day01.original.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Puzzles._2022;
+
+[Puzzle(2022, 01, CodeType.Original)]
+public class Day_01_Original : IPuzzle<List<int>>
+{
+	public List<int> Parse(PuzzleInput input) =>
+		input.Lines
+			.Split(string.Empty)
+			.Select(g => g.Select(int.Parse).Sum())
+			.ToList();
+
+	public string Part1(List<int> input) =>
+		input.Max().ToString();
+	public string Part2(List<int> input) =>
+		input.PartialSort(3, OrderByDirection.Descending)
+			.Sum()
+			.ToString();
+}
