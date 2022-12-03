6a694ddc04b76fedeee1be8dde81888f204a90bf
Stuart Turner <stuart@turner-isageek.com>
2022-12-03 12:03:31 -0600

    Y22 D01 F - New Puzzle project

---
+++ AdventOfCode.Common/Helpers.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Common;
+
+public static class Helpers
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static (int value, int numChars) AtoI(this ReadOnlySpan<byte> bytes)
+	{
+		// initial sign? also, track negative
+		var isNegSign = bytes[0] == '-';
+		if (isNegSign || bytes[0] == '+')
+			bytes = bytes[1..];
+
+		var value = 0;
+		var n = 0;
+		foreach (var t in bytes)
+		{
+			if (t == '\n')
+				break;
+
+			value = value * 10 + (t - '0');
+			n++;
+		}
+
+		return (isNegSign ? -value : value, n);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static (long value, int numChars) AtoL(this ReadOnlySpan<byte> bytes)
+	{
+		// initial sign? also, track negative
+		var isNegSign = bytes[0] == '-';
+		if (isNegSign || bytes[0] == '+')
+			bytes = bytes[1..];
+
+		var value = 0L;
+		var n = 0;
+		foreach (var t in bytes)
+		{
+			if (t == '\n')
+				break;
+
+			value = value * 10 + (t - '0');
+			n++;
+		}
+
+		return (isNegSign ? -value : value, n);
+	}
+}
---
+++ AdventOfCode.Puzzles/2022/day01.fastest.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Puzzles._2022;
+
+[Puzzle(2022, 01, CodeType.Fastest)]
+public class Day_01_Fastest : IPuzzle<(int, int, int)>
+{
+	public (int, int, int) Parse(PuzzleInput input)
+	{
+		Span<int> numbers = stackalloc int[3];
+		var elf = 0;
+
+		var span = new ReadOnlySpan<byte>(input.Bytes);
+		for (int i = 0; i < span.Length;)
+		{
+			if (span[i] == '\n')
+			{
+				if (elf > numbers[2])
+					numbers[2] = elf;
+				if (elf > numbers[1])
+					(numbers[1], numbers[2]) =
+						(elf, numbers[1]);
+				if (elf > numbers[0])
+					(numbers[0], numbers[1]) =
+						(elf, numbers[0]);
+
+				elf = 0;
+				i++;
+			}
+
+			var (value, numChars) = span[i..].AtoI();
+			i += numChars + 1;
+
+			elf += value;
+		}
+
+		return (span[0], span[1], span[2]);
+	}
+
+	public string Part1((int, int, int) input) =>
+		input.Item1.ToString();
+	public string Part2((int, int, int) input) =>
+		(input.Item1 + input.Item2 + input.Item3)
+			.ToString();
+}
