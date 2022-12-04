92433a4b67ab8424227e74bb956a8b250adc5419
Stuart Turner <stuart@turner-isageek.com>
2022-12-03 23:11:58 -0600

    Y22 D04 I

--- AdventOfCode.Puzzles/2022/day04.original.cs
+++ AdventOfCode.Puzzles/2022/day04.original.cs
@@ -3,12 +3,15 @@
 namespace AdventOfCode;
 
 [Puzzle(2022, 4, CodeType.Original)]
-public class Day_04_Original : IPuzzle
+public partial class Day_04_Original : IPuzzle
 {
+	[GeneratedRegex("(\\d+)-(\\d+),(\\d+)-(\\d+)")]
+	private static partial Regex RangeRegex();
+
 	public (string, string) Solve(PuzzleInput input)
 	{
-		var regex = new Regex(@"(\d+)-(\d+),(\d+)-(\d+)");
-		var part1 = input.Lines
+		var regex = RangeRegex();
+		var ranges = input.Lines
 			.Select(x => regex.Match(x))
 			.Select(m => (
 				int.Parse(m.Groups[1].Value),
@@ -17,7 +20,9 @@ public class Day_04_Original : IPuzzle
 				int.Parse(m.Groups[4].Value)))
 			.Select(x => (
 				Enumerable.Range(x.Item1, x.Item2 - x.Item1 + 1).ToList(),
-				Enumerable.Range(x.Item3, x.Item4 - x.Item3 + 1).ToList()))
+				Enumerable.Range(x.Item3, x.Item4 - x.Item3 + 1).ToList()));
+
+		var part1 = ranges
 			.Where(x =>
 			{
 				var intersect = x.Item1.Intersect(x.Item2).ToList();
@@ -27,16 +32,7 @@ public class Day_04_Original : IPuzzle
 			.Count()
 			.ToString();
 
-		var part2 = input.Lines
-			.Select(x => regex.Match(x))
-			.Select(m => (
-				int.Parse(m.Groups[1].Value),
-				int.Parse(m.Groups[2].Value),
-				int.Parse(m.Groups[3].Value),
-				int.Parse(m.Groups[4].Value)))
-			.Select(x => (
-				Enumerable.Range(x.Item1, x.Item2 - x.Item1 + 1).ToList(),
-				Enumerable.Range(x.Item3, x.Item4 - x.Item3 + 1).ToList()))
+		var part2 = ranges
 			.Where(x => x.Item1.Intersect(x.Item2).Any())
 			.Count()
 			.ToString();
