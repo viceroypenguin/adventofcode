1942dabdb1116360abd26aef26d2eaada3cfdb3a
Stuart Turner <stuart@turner-isageek.com>
2022-12-05 23:11:44 -0600

    Y22 D06

---
+++ AdventOfCode.Puzzles/2022/day06.original.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Puzzles._2022;
+
+[Puzzle(2022, 6, CodeType.Original)]
+public partial class Day_06_Original : IPuzzle
+{
+	public (string, string) Solve(PuzzleInput input)
+	{
+		var firstWindow = input.Text
+			.Window(4)
+			.Index()
+			.First(x => x.item.Distinct().Count() == 4)
+			.index;
+		var part1 = (firstWindow + 4).ToString();
+
+		firstWindow = input.Text
+			.Window(14)
+			.Index()
+			.First(x => x.item.Distinct().Count() == 14)
+			.index;
+		var part2 = (firstWindow + 14).ToString();
+		return (part1, part2);
+	}
+}
