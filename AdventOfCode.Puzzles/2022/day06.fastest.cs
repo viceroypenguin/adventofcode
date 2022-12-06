6113166fb62a022f0944da5a36786f8a3cfc53b4
Stuart Turner <stuart@turner-isageek.com>
2022-12-05 23:43:19 -0600

    Y22 D06 F

---
+++ AdventOfCode.Puzzles/2022/day06.fastest.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Puzzles._2022;
+
+[Puzzle(2022, 6, CodeType.Fastest)]
+public partial class Day_06_Fastest : IPuzzle
+{
+	public (string, string) Solve(PuzzleInput input) =>
+		(
+			GetIndex(input.Bytes, 4).ToString(),
+			GetIndex(input.Bytes, 14).ToString());
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	private static int GetIndex(ReadOnlySpan<byte> bytes, int numDistinct)
+	{
+		for (int i = numDistinct - 1; i < bytes.Length; i++)
+		{
+			for (int j = i - numDistinct + 1; j <= i; j++)
+			{
+				for (int k = j + 1; k <= i; k++)
+				{
+					if (bytes[j] == bytes[k])
+						goto next;
+				}
+			}
+
+			// if we make it through without goto, then we're done
+			return i + 1;
+
+next:
+			;
+		}
+
+		return bytes.Length;
+	}
+}
