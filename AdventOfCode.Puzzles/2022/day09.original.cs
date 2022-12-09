fdd92824fac659cecc283e0634c4ba6728d4b71b
Stuart Turner <stuart@turner-isageek.com>
2022-12-09 00:08:45 -0600

    Y22 D09 I - Move common code to functions

--- AdventOfCode.Puzzles/2022/day09.original.cs
+++ AdventOfCode.Puzzles/2022/day09.original.cs
@@ -14,23 +14,8 @@ public partial class Day_09_Original : IPuzzle
 			var cnt = int.Parse(l.AsSpan()[2..]);
 			for (int i = 0; i < cnt; i++)
 			{
-				head = l[0] switch
-				{
-					'U' => (head.x, head.y - 1),
-					'D' => (head.x, head.y + 1),
-					'L' => (head.x - 1, head.y),
-					'R' => (head.x + 1, head.y),
-				};
-
-				tail =
-					(head.x - tail.x, head.y - tail.y) switch
-					{
-						( > 1, _) => (head.x - 1, head.y),
-						( < -1, _) => (head.x + 1, head.y),
-						(_, > 1) => (head.x, head.y - 1),
-						(_, < -1) => (head.x, head.y + 1),
-						_ => (tail.x, tail.y),
-					};
+				head = MoveHead(head, l[0]);
+				tail = MoveFollower(head, tail);
 
 				tailVisited.Add(tail);
 			}
@@ -46,34 +31,13 @@ public partial class Day_09_Original : IPuzzle
 			var cnt = int.Parse(l.AsSpan()[2..]);
 			for (int i = 0; i < cnt; i++)
 			{
-				snake[0] = l[0] switch
-				{
-					'U' => (snake[0].x, snake[0].y - 1),
-					'D' => (snake[0].x, snake[0].y + 1),
-					'L' => (snake[0].x - 1, snake[0].y),
-					'R' => (snake[0].x + 1, snake[0].y),
-				};
+				snake[0] = MoveHead(snake[0], l[0]);
 
 				for (int j = 1; j < 10; j++)
 				{
-					head = snake[j - 1];
-					tail = snake[j];
+					var newTail = MoveFollower(snake[j - 1], snake[j]);
 
-					var newTail =
-						(head.x - tail.x, head.y - tail.y) switch
-						{
-							( > 1, > 1) => (head.x - 1, head.y - 1),
-							( > 1, < -1) => (head.x - 1, head.y + 1),
-							( < -1, > 1) => (head.x + 1, head.y - 1),
-							( < -1, < -1) => (head.x + 1, head.y + 1),
-							( > 1, _) => (head.x - 1, head.y),
-							( < -1, _) => (head.x + 1, head.y),
-							(_, > 1) => (head.x, head.y - 1),
-							(_, < -1) => (head.x, head.y + 1),
-							_ => (tail.x, tail.y),
-						};
-
-					if (newTail == tail)
+					if (newTail == snake[j])
 						break;
 
 					snake[j] = newTail;
@@ -87,4 +51,28 @@ public partial class Day_09_Original : IPuzzle
 
 		return (part1, part2);
 	}
+
+	static (int x, int y) MoveHead((int x, int y) head, char dir) =>
+		dir switch
+		{
+			'U' => (head.x, head.y - 1),
+			'D' => (head.x, head.y + 1),
+			'L' => (head.x - 1, head.y),
+			'R' => (head.x + 1, head.y),
+			_ => head,
+		};
+
+	static (int x, int y) MoveFollower((int x, int y) head, (int x, int y) tail) =>
+		(head.x - tail.x, head.y - tail.y) switch
+		{
+			( > 1, > 1) => (head.x - 1, head.y - 1),
+			( > 1, < -1) => (head.x - 1, head.y + 1),
+			( < -1, > 1) => (head.x + 1, head.y - 1),
+			( < -1, < -1) => (head.x + 1, head.y + 1),
+			( > 1, _) => (head.x - 1, head.y),
+			( < -1, _) => (head.x + 1, head.y),
+			(_, > 1) => (head.x, head.y - 1),
+			(_, < -1) => (head.x, head.y + 1),
+			_ => (tail.x, tail.y),
+		};
 }
