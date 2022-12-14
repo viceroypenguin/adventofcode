ae9b4b40a4704546d1b3b8102b8400ebc2989fdd
Stuart Turner <stuart@turner-isageek.com>
2022-12-14 10:23:36 -0600

    Y22 D14 I - use 2d array instead of Dictionary

--- AdventOfCode.Puzzles/2022/day14.original.cs
+++ AdventOfCode.Puzzles/2022/day14.original.cs
@@ -3,88 +3,115 @@
 [Puzzle(2022, 14, CodeType.Original)]
 public partial class Day_14_Original : IPuzzle
 {
-	public (string part1, string part2) Solve(PuzzleInput input) =>
-		(
-			RunSand(input.Lines, false).ToString(),
-			RunSand(input.Lines, true).ToString());
+	private record struct Line(int x1, int y1, int x2, int y2);
 
-	private static int RunSand(IEnumerable<string> lines, bool p2)
+	public (string part1, string part2) Solve(PuzzleInput input)
 	{
-		var map = new HashSet<(int x, int y)>();
-		foreach (var l in lines)
+		var (lines, maxX, maxY) = ParseLines(input.Lines);
+
+		return (
+			RunSand(lines, maxX, maxY, false).ToString(),
+			RunSand(lines, maxX, maxY, true).ToString());
+	}
+
+	private static (List<Line> lines, int maxX, int maxY) ParseLines(string[] lines)
+	{
+		var maxX = 0;
+		var maxY = 0;
+
+		return (
+			lines
+				.SelectMany(l => l.Split(" -> ")
+					.Select(ParseCoordinates)
+					.Do(x =>
+					{
+						if (x.x > maxX) maxX = x.x;
+						if (x.y > maxY) maxY = x.y;
+					})
+					.Window(2)
+					.Select(w => new Line(w[0].x, w[0].y, w[1].x, w[1].y))
+					.Distinct())
+				.ToList(),
+			maxX,
+			maxY);
+	}
+
+	private static (int x, int y) ParseCoordinates(string p)
+	{
+		var span = p.AsSpan();
+		var (x, n) = span.AtoI();
+		var (y, _) = span[(n + 1)..].AtoI();
+		return (x, y);
+	}
+
+	private static int RunSand(List<Line> lines, int maxX, int maxY, bool p2)
+	{
+		maxY += 2;
+		maxX = 510 + maxY;
+		var map = new byte[maxX, maxY + 1];
+
+		foreach (var (x1, y1, x2, y2) in lines)
 		{
-			foreach (var (start, end) in l.Split(" -> ")
-				.Select(x => x.Split(','))
-				.Select(s => (x: int.Parse(s[0]), y: int.Parse(s[1])))
-				.Window(2)
-				.Select(w => (w[0], w[1])))
+			if (x1 == x2)
 			{
-				if (start.x == end.x)
-				{
-					var min = int.Min(start.y, end.y);
-					var max = int.Max(start.y, end.y);
+				var min = int.Min(y1, y2);
+				var max = int.Max(y1, y2);
 
-					for (int j = min; j <= max; j++)
-						map.Add((start.x, j));
-				}
-				else
-				{
-					var min = int.Min(start.x, end.x);
-					var max = int.Max(start.x, end.x);
+				for (int j = min; j <= max; j++)
+					map[x1, j] = (byte)'#';
+			}
+			else
+			{
+				var min = int.Min(x1, x2);
+				var max = int.Max(x1, x2);
 
-					for (int j = min; j <= max; j++)
-						map.Add((j, start.y));
-				}
+				for (int j = min; j <= max; j++)
+					map[j, y1] = (byte)'#';
 			}
 		}
 
-		var maxY = map.Select(k => k.y).Max();
 		if (p2)
 		{
-			maxY += 2;
 			for (int x = 490 - maxY; x < 510 + maxY; x++)
-				map.Add((x, maxY));
+				map[x, maxY] = (byte)'#';
 			maxY++;
 		}
 
 		var n = 0;
 		while (true)
 		{
-			var pos = (x: 500, y: 0);
+			var (x, y) = (500, 0);
 
-			if (map.Contains(pos))
+			if (map[x, y] != 0)
 				return n;
 
 			while (true)
 			{
-				var newPos = (pos.x, y: pos.y + 1);
-				if (newPos.y > maxY)
+				if (y + 1 >= maxY)
 					return n;
 
-				if (!map.Contains(newPos))
+				if (map[x, y + 1] == 0)
 				{
-					pos = newPos;
+					(x, y) = (x, y + 1);
 					continue;
 				}
 
-				newPos = (pos.x - 1, pos.y + 1);
-				if (!map.Contains(newPos))
+				if (map[x - 1, y + 1] == 0)
 				{
-					pos = newPos;
+					(x, y) = (x - 1, y + 1);
 					continue;
 				}
 
-				newPos = (pos.x + 1, pos.y + 1);
-				if (!map.Contains(newPos))
+				if (map[x + 1, y + 1] == 0)
 				{
-					pos = newPos;
+					(x, y) = (x + 1, y + 1);
 					continue;
 				}
 
 				break;
 			}
 
-			map.Add(pos);
+			map[x, y] = (byte)'O';
 			n++;
 		}
 	}
