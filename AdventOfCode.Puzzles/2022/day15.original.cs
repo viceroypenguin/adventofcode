56a6a1e21e1682c54b16eb913abbcd00c58d465f
Stuart Turner <stuart@turner-isageek.com>
2022-12-15 08:45:53 -0600

    Y22 D15 I - Use range combining for p1

--- AdventOfCode.Puzzles/2022/day15.original.cs
+++ AdventOfCode.Puzzles/2022/day15.original.cs
@@ -3,9 +3,16 @@
 [Puzzle(2022, 15, CodeType.Original)]
 public partial class Day_15_Original : IPuzzle
 {
+	[GeneratedRegex("Sensor at x=(?<sx>-?\\d+), y=(?<sy>-?\\d+): closest beacon is at x=(?<bx>-?\\d+), y=(?<by>-?\\d+)")]
+	private static partial Regex SensorRegex();
+
+	private record struct Sensor(
+		int sx, int sy,
+		int bx, int by,
+		int dist);
 	public (string part1, string part2) Solve(PuzzleInput input)
 	{
-		var regex = new Regex(@"Sensor at x=(?<sx>-?\d+), y=(?<sy>-?\d+): closest beacon is at x=(?<bx>-?\d+), y=(?<by>-?\d+)");
+		var regex = SensorRegex();
 		var data = input.Lines
 			.Select(l => regex.Match(l))
 			.Select(m => (
@@ -13,25 +20,12 @@ public partial class Day_15_Original : IPuzzle
 				sy: int.Parse(m.Groups["sy"].Value),
 				bx: int.Parse(m.Groups["bx"].Value),
 				by: int.Parse(m.Groups["by"].Value)))
-			.Select(s => (
+			.Select(s => new Sensor(
 				s.sx, s.sy, s.bx, s.by,
 				dist: Math.Abs(s.sx - s.bx) + Math.Abs(s.sy - s.by)))
 			.ToList();
 
-		const int Row = 2_000_000;
-		var minX = data
-			.Select(s => s.sx - (s.dist - Math.Abs(Row - s.sy)))
-			.Min();
-		var maxX = data
-			.Select(s => s.sx + (s.dist - Math.Abs(Row - s.sy)))
-			.Max();
-
-		var points = Enumerable.Range(minX, maxX - minX + 1)
-			.Where(x => !data.Any(s => (s.bx, s.by) == (x, Row)))
-			.Where(x => data.Any(s => Math.Abs(x - s.sx) + Math.Abs(Row - s.sy) <= s.dist))
-			.Count();
-
-		var part1 = points.ToString();
+		var part1 = DoPart1(data).ToString();
 
 		const int MapSize = 4_000_000;
 		var point = Enumerable.Range(0, MapSize + 1)
@@ -57,6 +51,42 @@ public partial class Day_15_Original : IPuzzle
 
 		var part2 = (point.x * 4_000_000L + point.y).ToString();
 		return (part1, part2);
+	}
+
+	private static int DoPart1(List<Sensor> sensors)
+	{
+		const int Row = 2_000_000;
+		var ranges = sensors
+			.Select(s => (s.sx, distToY: s.dist - Math.Abs(Row - s.sy)))
+			.Where(s => s.distToY >= 0)
+			.Select(s => (minX: s.sx - s.distToY, maxX: s.sx + s.distToY))
+			.OrderBy(s => s.minX);
+
+		var notPossible = new List<(int min, int max)>();
+		foreach (var (minX, maxX) in ranges)
+		{
+			if (notPossible.Count == 0
+				|| minX > notPossible[^1].max + 1)
+			{
+				notPossible.Add((minX, maxX));
+			}
+			else
+			{
+				notPossible[^1] = (
+					notPossible[^1].min,
+					Math.Max(maxX, notPossible[^1].max));
+			}
+		}
+
+		var beacons = sensors
+			.Where(s => s.by == Row)
+			.Select(s => s.bx)
+			.Distinct()
+			.ToList();
+
+		return notPossible
+			.Select(r => (r.min, max: r.max - beacons.Count(b => b.Between(r.min, r.max))))
+			.Sum(r => r.max - r.min + 1);
 	}
 }
 
