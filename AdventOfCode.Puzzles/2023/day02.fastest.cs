cce618445ac411aabb4a2f79d099a220d48bab09
Stuart Turner <stuart@turner-isageek.com>
2023-12-02 07:06:46 -0600

    Y23 D02 F

---
+++ AdventOfCode.Puzzles/2023/day02.fastest.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode.Puzzles._2023;
+
+[Puzzle(2023, 02, CodeType.Fastest)]
+public partial class Day_02_Fastest : IPuzzle
+{
+	public (string, string) Solve(PuzzleInput input)
+	{
+		var part1 = 0;
+		var part2 = 0;
+		var span = input.Span;
+
+		while (span.Length > 0)
+		{
+			var (id, n) = span.Slice(5).AtoI();
+			span = span.Slice(n + 7);
+
+			var maxRed = 0;
+			var maxGreen = 0;
+			var maxBlue = 0;
+
+			while (true)
+			{
+				(var num, n) = span.AtoI();
+				span = span.Slice(n + 1);
+
+				switch (span[0])
+				{
+					case (byte)'r':
+					{
+						maxRed = Math.Max(maxRed, num);
+						span = span.Slice(3);
+						break;
+					}
+					case (byte)'b':
+					{
+						maxBlue = Math.Max(maxBlue, num);
+						span = span.Slice(4);
+						break;
+					}
+					case (byte)'g':
+					{
+						maxGreen = Math.Max(maxGreen, num);
+						span = span.Slice(5);
+						break;
+					}
+				}
+
+				if (span[0] == (byte)'\n')
+					break;
+
+				span = span.Slice(2);
+			}
+
+			span = span.Slice(1);
+
+			if (maxRed <= 12
+				&& maxGreen <= 13
+				&& maxBlue <= 14)
+			{
+				part1 += id;
+			}
+
+			part2 += maxRed * maxGreen * maxBlue;
+		}
+
+		return (part1.ToString(), part2.ToString());
+	}
+}
