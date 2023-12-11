4b6fc39bfd6550c676f5ae297d3770f84a2a303d
Stuart Turner <stuart@turner-isageek.com>
2023-12-11 10:18:07 -0600

    Y23 D11 F

--- AdventOfCode.Common/Extensions/SpanExtensions.cs
+++ AdventOfCode.Common/Extensions/SpanExtensions.cs
@@ -2,10 +2,8 @@
 
 public static class SpanExtensions
 {
-	public static SpanByteLineEnumerator EnumerateLines(this ReadOnlySpan<byte> span)
-	{
-		return new SpanByteLineEnumerator(span);
-	}
+	public static SpanByteLineEnumerator EnumerateLines(this ReadOnlySpan<byte> span) => new(span);
+	public static SpanByteLineLengthEnumerator EnumerateLines(this ReadOnlySpan<byte> span, int length) => new(span, length);
 }
 
 /// <summary>
@@ -58,6 +56,71 @@ public ref struct SpanByteLineEnumerator
 		{
 			Current = remaining.Slice(0, idx);
 			_remaining = remaining.Slice(idx + 1);
+		}
+		else
+		{
+			// We've reached EOF, but we still need to return 'true' for this final
+			// iteration so that the caller can query the Current property once more.
+
+			Current = remaining;
+			_remaining = default;
+			_isEnumeratorActive = false;
+		}
+
+		return true;
+	}
+}
+
+/// <summary>
+/// Enumerates the lines of a <see cref="ReadOnlySpan{Char}"/>.
+/// </summary>
+/// <remarks>
+/// To get an instance of this type, use <see cref="MemoryExtensions.EnumerateLines(ReadOnlySpan{char})"/>.
+/// </remarks>
+public ref struct SpanByteLineLengthEnumerator
+{
+	private readonly int _length;
+	private ReadOnlySpan<byte> _remaining;
+	private bool _isEnumeratorActive;
+
+	internal SpanByteLineLengthEnumerator(ReadOnlySpan<byte> buffer, int length)
+	{
+		_length = length;
+		_remaining = buffer;
+		Current = default;
+		_isEnumeratorActive = true;
+	}
+
+	/// <summary>
+	/// Gets the line at the current position of the enumerator.
+	/// </summary>
+	public ReadOnlySpan<byte> Current { get; private set; }
+
+	/// <summary>
+	/// Returns this instance as an enumerator.
+	/// </summary>
+	public readonly SpanByteLineLengthEnumerator GetEnumerator() => this;
+
+	/// <summary>
+	/// Advances the enumerator to the next line of the span.
+	/// </summary>
+	/// <returns>
+	/// True if the enumerator successfully advanced to the next line; false if
+	/// the enumerator has advanced past the end of the span.
+	/// </returns>
+	public bool MoveNext()
+	{
+		if (!_isEnumeratorActive)
+		{
+			return false; // EOF previously reached or enumerator was never initialized
+		}
+
+		var remaining = _remaining;
+
+		if ((uint)_length < (uint)remaining.Length)
+		{
+			Current = remaining.Slice(0, _length);
+			_remaining = remaining.Slice(_length + 1);
 		}
 		else
 		{
---
+++ AdventOfCode.Puzzles/2023/day11.fastest.cs
@@ -0,0 +1,73 @@
+﻿namespace AdventOfCode.Puzzles._2023;
+
+[Puzzle(2023, 11, CodeType.Fastest)]
+public sealed partial class Day_11_Fastest : IPuzzle
+{
+	public (string, string) Solve(PuzzleInput input)
+	{
+		var span = input.Span;
+
+		var width = span.IndexOf((byte)'\n');
+		Span<int> cols = stackalloc int[width];
+
+		var part1 = 0;
+		var part2 = 0L;
+
+		var galaxies = 0;
+		var yDistanceP1 = 0;
+		var yDistanceP2 = 0L;
+		foreach (var line in span.EnumerateLines(width))
+		{
+			span = line;
+
+			var x = 0;
+			var gap = true;
+
+			while (true)
+			{
+				var n = span.IndexOf((byte)'#');
+				if (n < 0)
+					break;
+
+				part1 += yDistanceP1;
+				part2 += yDistanceP2;
+
+				gap = false;
+				galaxies++;
+				cols[x + n]++;
+				x += n + 1;
+				span = span.Slice(n + 1);
+			}
+
+			yDistanceP1 += galaxies;
+			yDistanceP2 += galaxies;
+
+			if (gap)
+			{
+				yDistanceP1 += galaxies;
+				yDistanceP2 += galaxies * 999_999L;
+			}
+		}
+
+		galaxies = 0;
+		var xDistanceP1 = 0;
+		var xDistanceP2 = 0L;
+		foreach (var col in cols)
+		{
+			part1 += xDistanceP1 * col;
+			part2 += xDistanceP2 * col;
+
+			galaxies += col;
+			xDistanceP1 += galaxies;
+			xDistanceP2 += galaxies;
+
+			if (col == 0)
+			{
+				xDistanceP1 += galaxies;
+				xDistanceP2 += galaxies * 999_999L;
+			}
+		}
+
+		return (part1.ToString(), part2.ToString());
+	}
+}
