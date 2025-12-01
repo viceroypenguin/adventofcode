87479d7ecba1de288038a929e87bb79804b49690
Stuart Turner <stuart@turner-isageek.com>
2025-11-30 23:32:56 -0600

    Y25 D01

--- AdventOfCode.Puzzles/2025/day01.original.cs
+++ AdventOfCode.Puzzles/2025/day01.original.cs
@@ -5,10 +5,26 @@ public class Day_01_Original : IPuzzle
 {
 	public (string, string) Solve(PuzzleInput input)
 	{
-		var part1 = string.Empty;
+		var value = 50;
+		var part1 = 0;
+		var part2 = 0;
 
-		var part2 = string.Empty;
+		foreach (var l in input.Lines)
+		{
+			var num = int.Parse(l.AsSpan()[1..]);
 
-		return (part1, part2);
+			(value, part2) = l[0] switch
+			{
+				'R' => ((value + num) % 100, part2 + ((value + num) / 100)),
+				_ => ((value - num + 10_000) % 100, part2 + ((num - value) / 100) + (value > 0 && num >= value ? 1 : 0)),
+			};
+
+#pragma warning disable CA1508 // Avoid dead conditional code
+			if (value == 0)
+				part1++;
+#pragma warning restore CA1508 // Avoid dead conditional code
+		}
+
+		return (part1.ToString(), part2.ToString());
 	}
 }
