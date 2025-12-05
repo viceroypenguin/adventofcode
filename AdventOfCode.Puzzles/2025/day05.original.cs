60ab5aa7870f33865783ca968a6967e349c4c127
Stuart Turner <stuart@turner-isageek.com>
2025-12-05 09:55:05 -0600

    Y25 D05

--- AdventOfCode.Puzzles/2025/day05.original.cs
+++ AdventOfCode.Puzzles/2025/day05.original.cs
@@ -12,32 +12,61 @@ public partial class Day_05_Original : IPuzzle
 
 		var idRanges = parts[0]
 			.Select(l => RangeRegex.Match(l))
-			.Select(m => (lo: long.Parse(m.Groups[1].Value), hi: long.Parse(m.Groups[2].Value)))
+			.Select(m => (lo: long.Parse(m.Groups["lo"].Value), hi: long.Parse(m.Groups["hi"].Value)))
+			.Order()
+			.Merge()
 			.ToList();
 
 		var part1 = parts[1]
 			.Select(long.Parse)
-			.Count(i => idRanges.Exists(r => i.Between(r.lo, r.hi)));
-
-		var (part2, _) = idRanges
-			.Order()
-			.Aggregate(
-				(sum: 0L, max: long.MinValue),
-				(x, y) =>
-				{
-					var ((sum, max), (lo, hi)) = (x, y);
-
-					if (max >= lo)
-						lo = max + 1;
-
-					if (max >= hi)
-						hi = max;
-
-					sum += hi - lo + 1;
-					return (sum, hi);
-				}
+			.Count(
+				i => idRanges.BinarySearch(
+					(i, long.MinValue),
+					Comparer<(long Lo, long Hi)>.Create(CompareIdToRange)
+				) >= 0
 			);
 
+		var part2 = idRanges.Sum(x => x.Hi - x.Lo + 1);
+
 		return (part1.ToString(), part2.ToString());
+	}
+
+	private static int CompareIdToRange((long Lo, long Hi) x, (long Lo, long Hi) y)
+	{
+		var id = y.Lo;
+		if (id < x.Lo)
+			return 1;
+		if (id > x.Hi)
+			return -1;
+		return 0;
+	}
+}
+
+file static class Extensions
+{
+	public static IEnumerable<(long Lo, long Hi)> Merge(
+		this IEnumerable<(long Lo, long Hi)> source
+	)
+	{
+		using var enumerator = source.GetEnumerator();
+
+		if (!enumerator.MoveNext())
+			yield break;
+
+		var (lo, hi) = enumerator.Current;
+
+		while (enumerator.MoveNext())
+		{
+			if (enumerator.Current.Lo > hi + 1)
+			{
+				yield return (lo, hi);
+				(lo, hi) = enumerator.Current;
+				continue;
+			}
+
+			hi = Math.Max(hi, enumerator.Current.Hi);
+		}
+
+		yield return (lo, hi);
 	}
 }
