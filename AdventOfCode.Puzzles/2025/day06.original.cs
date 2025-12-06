106abc97c45137a703795346ad09ce6df151d83e
Stuart Turner <stuart@turner-isageek.com>
2025-12-06 10:48:08 -0600

    Y25 D06

--- AdventOfCode.Puzzles/2025/day06.original.cs
+++ AdventOfCode.Puzzles/2025/day06.original.cs
@@ -5,61 +5,45 @@ public partial class Day_06_Original : IPuzzle
 {
 	public (string, string) Solve(PuzzleInput input)
 	{
-		var row1 = input.Lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
-		var row2 = input.Lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
-		var row3 = input.Lines[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
-		var row4 = input.Lines[3].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
-		var row5 = input.Lines[4].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+		var actions = input.Lines[^1].Index()
+			.Where(x => x.Item != ' ')
+			.ToList();
 
-		var part1 = row5
-			.EquiZip(row1.EquiZip(row2, row3, row4))
-			.Sum(x => x.Item1 switch
-			{
-				"*" => x.Item2.Item1 * x.Item2.Item2 * x.Item2.Item3 * x.Item2.Item4,
-				_ => x.Item2.Item1 + x.Item2.Item2 + x.Item2.Item3 + x.Item2.Item4,
-			});
-
-		var rows = input.Lines;
-
-		var part2 = 0L;
-
-		var action = '+';
-		var startColumn = 0;
-
-		for (var i = 0; i <= rows[0].Length; i++)
-		{
-			if (i == rows[0].Length || rows.All(l => l[i] == ' '))
-			{
-				var numbers = new long[i - startColumn];
-
-				for (var j = 0; j < numbers.Length; j++)
-				{
-					var number = 0L;
-
-					for (var k = 0; k < rows.Length - 1; k++)
-					{
-						if (rows[k][j + startColumn] != ' ')
-							number = (number * 10) + rows[k][j + startColumn] - '0';
-					}
-
-					numbers[j] = number;
-				}
-
-				part2 += action switch
+		var part1 = actions.Select(x => x.Item)
+			.EquiZip(
+				input.Lines.SkipLast(1)
+					.Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+						.Select(long.Parse)
+						.ToList()
+					)
+					.Transpose(),
+				(action, numbers) => action switch
 				{
 					'*' => numbers.Aggregate(1L, (a, b) => a * b),
 					_ => numbers.Sum(),
-				};
+				}
+			)
+			.Sum();
 
-				continue;
-			}
+		var indices = actions.Select(x => x.Index - 1).ToHashSet();
 
-			if (rows[^1][i] != ' ')
-			{
-				action = rows[^1][i];
-				startColumn = i;
-			}
-		}
+		var part2 = actions.Select(x => x.Item)
+			.EquiZip(
+				input.Lines.SkipLast(1)
+					.Select(l => l.Segment((_, i) => indices.Contains(i)))
+					.Transpose()
+					.Select(
+						x => x.Transpose()
+							.Where(x => !x.All(c => c == ' '))
+							.Select(x => long.Parse(new string(x.ToArray())))
+					),
+				(action, numbers) => action switch
+				{
+					'*' => numbers.Aggregate(1L, (a, b) => a * b),
+					_ => numbers.Sum(),
+				}
+			)
+			.Sum();
 
 		return (part1.ToString(), part2.ToString());
 	}
